5d03f95a92fec47512d1143c06eac42947cf1833
Jon <woosetrax@gmail.com>
2013-03-19 11:30:22 -0700

    capify

---
+++ config/deploy.rb
@@ -0,0 +1,47 @@
+require 'rvm/capistrano'
+require 'bundler/capistrano'
+
+set :application, "pidginpost.com"
+set :repository,  "~/email_dev"
+set :scm, :git
+
+# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "ec2key.pem")]
+set :user, 'ubuntu'
+set :use_sudo, false
+set :deploy_to, "/var/www/#{application}"
+set :deploy_via, :remote_cache
+
+role :web, "pidginpost.com"
+role :app, "pidginpost.com"
+role :db, "pidginpost.com", :primary => true
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
+
+after "deploy", "deploy:bundle_gems"
+after "deploy:bundle_gems", "deploy:restart"
+
+namespace :deploy do
+  task :bundle_gems do
+    run "cd #{deploy_to}/current && source $HOME/.bash_profile && bundle install"
+  end
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{File.join(current_path, 'tmp', 'restart.txt')}"
+  end
+end
